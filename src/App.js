edc426160059744c1b18b9b59390c38dffd92bfc
AhmedNagii <36850727+AhmedNagii@users.noreply.github.com>
2022-07-27 20:25:37 +0200

    complete project

--- src/App.js
+++ src/App.js
@@ -1,52 +1,62 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 function App() {
+  const STARTING_TIME = 5;
+
+  const textAreaRef = useRef(null);
   const [input, setInput] = useState("");
   const [wordsCount, setWordsCount] = useState(0);
-  const [remainingTime, setRemainingTime] = useState(5);
+  const [remainingTime, setRemainingTime] = useState(STARTING_TIME);
   const [running, setRunning] = useState(false);
-  const [hasEnded, setHasEnded] = useState(false);
 
   function handelChange(event) {
     setInput(event.target.value);
   }
 
   function countWords(myText) {
-
     const numOfwords = myText
       .trim()
       .split(" ")
       .filter((word) => word !== "").length;
-    setWordsCount(numOfwords);
+    return numOfwords;
   }
 
-  function restartGame (){
-    setInput("")
-    setWordsCount(0)
-    setRemainingTime(5)
-    
+  function startGame() {
+    setRunning(true);
+    setWordsCount(0);
+    setRemainingTime(STARTING_TIME);
+    setInput("");
   }
- useEffect(() => {
-  if(running && remainingTime > 0 ) {
-    setTimeout(() => {
-      setRemainingTime(time => time - 1)
-    }, 1000)
-} else{
-  setRunning(false)
-  countWords(input)
-  
-}
-  
-  },[remainingTime , running]);
+  function endGame() {
+    setRunning(false);
+    setWordsCount(countWords(input));
+  }
+
+  useEffect(() => {
+    if (running && remainingTime > 0) {
+      textAreaRef.current.focus();
+      setTimeout(() => {
+        setRemainingTime((time) => time - 1);
+      }, 1000);
+    } else {
+      endGame();
+    }
+  }, [remainingTime, running]);
 
   return (
     <div className="App">
-      <h1>Hello world</h1>
-      <textarea onChange={handelChange} />
+      <h1>How fast do you type</h1>
+      <textarea ref={textAreaRef} disabled={!running} onChange={handelChange} />
       <h4>Remaining time: {remainingTime}</h4>
-      <button onClick={() =>
-       {setRunning(true)}   }>{hasEnded? "Restart" : "start"} </button>
+      <button
+        disabled={running}
+        onClick={() => {
+          remainingTime === 0 ? startGame() : setRunning(true);
+        }}
+      >
+        {remainingTime === 0 ? "Restart" : "start"}{" "}
+      </button>
       <h1>Word count: {wordsCount}</h1>
     </div>
   );
